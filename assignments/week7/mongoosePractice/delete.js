b9278a377a8d150a9efe40b26cb0d56f95c56a22
JJSiri <JJSiripatt@gmail.com>
2020-12-16 11:59:17 -0800

    Started To-Do-Application

---
+++ assignments/week7/mongoosePractice/delete.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const db = "mongodb+srv://mongoosePracticeUser:mongoosePracticeUser@cluster0.rynoa.mongodb.net/genericDatabase?retryWrites=true&w=majority";
+
+const options = {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true};
+
+let connection = mongoose.connect(db, options, (error) => {
+    if (error) {
+        console.log("There was an error!", error);
+    } else {
+        console.log("Successfully connected to MongoDB Database genericDatabase.");
+    }
+});
+
+let exampleSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    created: String,
+    age: Number,
+    accountClosed: Boolean
+});
+
+let exampleModel = new mongoose.model("firstcollections", exampleSchema);
+
+
+// Finds a specific document by its ID, and deletes the document from the database, and returns either an error or results. Note results will have a copy of the deleted document.
+exampleModel.findByIdAndDelete("5fca765ac9714d5624f5c96d", (error, results) => {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("Successfully deleted the following document from the database: ", results);
+    }
+});
